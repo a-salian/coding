2422c4fccd236e548ad6413a525e4571469b6e0f
Sivashankar C <sivashankarchandrappa.sangamone@gmail.com>
2025-03-23 13:15:56 +0530

    Factorial function

--- 1.py
+++ 1.py
@@ -2,3 +2,11 @@ def square(num1):
     n1=num1
     square1=n1*n1
     print(square1)
+    
+def fact(num1):
+    fact1=1
+    for i in range(1,num1+1,1):
+        fact1=fact1*i
+    print(fact1)
+
+result1=fact(6)
